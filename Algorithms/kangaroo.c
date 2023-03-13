4c49fd3b2f964ffe22fac24f16113dc97e3cfa4f
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-03-13 10:06:05 -0300

    Update kangaroo.c

--- Algorithms/kangaroo.c
+++ Algorithms/kangaroo.c
@@ -1,22 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(void)
+int main() 
 {
-    int x1,x2,v1,v2;
+    int x1, v1, x2, v2;
     scanf("%d %d %d %d", &x1, &v1, &x2, &v2);
 
-    if(x2 + v2 == x1 + v1)
-    {
-        if(x2 > x1 && v2 > v1)
-        {
-            printf("NO\n");
-        }else if(x2 > x1 && v2 < v1|| x2 < x1 && v2 > v1)
-        {
-            printf("YES\n");
-        }
-    }else if(x2 = v2 != x1 + v1)
+    if (v2 >= v1) 
     {
         printf("NO\n");
+    }else{
+        int dx = x2 - x1;
+        int dv = v1 - v2;
+        if (dx % dv == 0) {
+            printf("YES\n");
+        }else{
+            printf("NO\n");
+        }
     }
+    
     return 0;
 }
