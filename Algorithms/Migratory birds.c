d5782c5295db9f96e6783b5349b2fcad3a95bd73
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-03-13 19:34:12 -0300

    Update Migratory birds.c

--- Algorithms/Migratory birds.c
+++ Algorithms/Migratory birds.c
@@ -1,46 +1,40 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int comparador(const void* a, const void* b)
+int verificador(int size, int *arr)
 {
-    return (*(int*)a - *(int*)b);
-}
-
-int verificador(int size, int *arr) 
-{
-    qsort(arr, size, sizeof(int), comparador);
-
     int max_freq = 0;
-    int freq = 1;
-    int mode = arr[0];
+    int mode = 0;
 
-    for (int i = 1; i < size; i++) {
-        if (arr[i] == arr[i-1]) {
-            freq++;
-        } else {
-            if (freq > max_freq) {
-                max_freq = freq;
-                mode = arr[i-1];
+    for(int i = 0;i < size;i++)
+    {
+        int freq = 0;
+        for(int j = i + 1;j < size;j++)
+        {
+            if(arr[j] == arr[i])
+            {
+                freq++;
             }
-            freq = 1;
         }
-    }
-
-    if (freq > max_freq) {
-        mode = arr[size-1];
+        if(freq > max_freq)
+        {
+            max_freq = freq;
+            mode = arr[i];
+        }
     }
 
     return mode;
 }
 
-int main(void) {
+int main(void)
+{
     int size;
 
     scanf("%d",&size);
 
     int arr[size];
 
-    for(int i = 0; i < size; i++) {
+    for(int i = 0;i < size;i++)
+    {
         scanf("%d",&arr[i]);
     }
 
