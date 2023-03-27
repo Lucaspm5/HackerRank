d979891099b5bc70b7227aa4bcf4a9bbf3f6a16c
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-03-27 13:50:03 -0300

    Update teste.c

--- Algorithms/teste.c
+++ Algorithms/teste.c
@@ -1,37 +1,38 @@
 #include <stdio.h>
 
-void classe(const char* name, const char* sname, long int idade, int score)
-{
-    char caracter;
-    if(score >= 90 && score <= 100)
-        caracter = 'O';
-    else if(score >= 80 && score < 90)
-        caracter = 'A';
-    else if(score >= 70 && score < 80)
-        caracter = 'B';
-    else if(score >= 60 && score < 70)
-        caracter = 'C';
-    else if(score >= 50 && score < 60)
-        caracter = 'D';
-    else
-        caracter = 'F';
-
-    printf("Name: %s, %s\nID: %ld\nGRADE: %c\n", sname, name, idade, caracter);
-}
-
 int main(void)
 {
-    int score1, score2, numbertest;
-    char name[10000], sname[10000];
-    long int number;
+    int n;
+    scanf("%d", &n);
 
-    scanf("%s %s %ld", name, sname, &number);
-    scanf("%d",&numbertest);
-    scanf("%d %d", &score1, &score2);
+    int scores[n];
 
-    int scoretotal = (int)(score1 + score2)/2;
+    for (int i = 0; i < n; i++){
+        scanf("%d", &scores[i]);
+    }
 
-    classe(name, sname, number, scoretotal);
+    int max_score = scores[0];
+    int max_count = 0;
+
+    for (int i = 1; i < n; i++){
+        if (scores[i] > max_score){
+            max_score = scores[i];
+            max_count++;
+        }
+    }
+
+    int min_score = scores[0];
+    int min_count = 0;
+
+    for(int i = 1; i < n;i++)
+    {
+        if(scores[i] < min_score){
+            min_score = scores[i];
+            min_count++;
+        }
+    }
+
+    printf("%d %d\n", max_count,min_count);
 
     return 0;
 }
